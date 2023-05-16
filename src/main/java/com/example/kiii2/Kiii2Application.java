64cccf73d7782ccb7fb2fc68456dd6a3b716a133
Fedora Lepcheska <flepceska@gmail.com>
2023-05-16 16:00:22 +0200

    initial

--- src/main/java/com/example/kiii2/Kiii2Application.java
+++ src/main/java/com/example/kiii2/Kiii2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Kiii2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Kiii2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Kiii2Application.class, args);
+    }
 
 }
