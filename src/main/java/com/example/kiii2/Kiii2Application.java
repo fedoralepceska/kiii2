bc48b640ad19302685e50f11c11ff2d050f32a63
Fedora Lepcheska <flepceska@gmail.com>
2023-05-16 15:53:58 +0200

    Initial commit

---
+++ src/main/java/com/example/kiii2/Kiii2Application.java
@@ -0,0 +1,13 @@
+package com.example.kiii2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kiii2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kiii2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiii2/Kiii2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiii2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kiii2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
